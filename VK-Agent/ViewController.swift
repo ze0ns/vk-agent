5930fb2c25e553e5635eacda573b440e83d880ce
zeons <oao@i-cio.ru>
2021-07-06 11:41:45 +0300

    Initial Commit

---
+++ VK-Agent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK-Agent
+//
+//  Created by Aleksandr Oschepkov on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
