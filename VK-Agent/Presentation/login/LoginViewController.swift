7a359dc835553df42c73a5488f89ec2520771771
zeons <oao@i-cio.ru>
2021-07-07 16:10:57 +0300

    Экран входа в приложение готов

--- VK-Agent/Presentation/login/LoginViewController.swift
+++ VK-Agent/Presentation/login/LoginViewController.swift
@@ -9,28 +9,53 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
-    @IBOutlet weak var titleImageView: UIImageView!
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var titleLableUsername: UILabel!
-    @IBOutlet weak var titlePassword: UILabel!
-    @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet private weak var titleImageView: UIImageView!
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var titleLableUsername: UILabel!
+    @IBOutlet private weak var titlePassword: UILabel!
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginButton: UIButton!
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        loginButton.setTitle("Log in", for: .normal)
-        loginButton.layer.cornerRadius = 8
-        loginButton.backgroundColor = .yellow
+        setViews()
     
     }
-
-    @IBAction func loginButtonAction(_ sender: UIButton) {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //Подписываемся на 2 уведомления: Одно при появлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        //Второе - когда клавиатура пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+        //Необходимо отписаться от Нитификационного центра
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @IBAction private func loginButtonAction(_ sender: UIButton) {
+        guard
+        let login = usernameTextField.text,
+        let password = passwordTextField.text
+        else {
+            print("Error login or password")
+            return
+        }
+        if (login == "admin") && (password == "1234"){
+            print("Добро пожаловать")
+        } else {
+            print("ошибка")
+        }
+        
+        
+    }
+    
     //Клавиатура появляется
-    @objc func keyboardWasShow(notification:Notification){
+    @objc private func keyboardWasShow(notification:Notification){
         
         //Получаем размер клавиатуры
         let info = notification.userInfo! as NSDictionary
@@ -42,10 +67,22 @@ final class LoginViewController: UIViewController {
         scrollView?.scrollIndicatorInsets = contentInsets
     }
     //Клавиатура исчезает
-    @objc func keyboardWillBeHidden(notification:Notification){
+    @objc private func keyboardWillBeHidden(notification:Notification){
         //Устанавливаем отступ равный 0
         let contentInsets = UIEdgeInsets.zero
         scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    @objc private func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+    }
+    private func setViews(){
+        loginButton.layer.cornerRadius = 8
+        
+        
+        //Отслеживаем нажатие на экран - на наш СкролВью
+        let tapGestruru = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView.addGestureRecognizer(tapGestruru)
     }
     
 }
