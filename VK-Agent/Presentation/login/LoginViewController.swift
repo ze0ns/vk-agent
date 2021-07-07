b51bcadb836f80194ededdea6c0c3642dfe931c5
ze0ns <zeons@mail.ru>
2021-07-07 06:04:54 +0300

    Пробуем связать наши объекты с кодом

--- VK-Agent/Presentation/login/LoginViewController.swift
+++ VK-Agent/Presentation/login/LoginViewController.swift
@@ -1,27 +1,17 @@
-//
-//  LoginViewController.swift
-//  VK-Agent
-//
-//  Created by Aleksandr Oschepkov on 06.07.2021.
-//
-
-import UIKit
 //final обозначает что от этого класса нельзя наследоваться
 //полезно для оптимизации приложения
+//frame - размеры относительно родительской вьюшки (x,y,h,w)
+//bounds = (x,y,h,w)
+// CGRect - Задаем координаты и размеры нашей вьюшки
+//    Constraints - позволяет задаваеть какаие отступы делать для вьюшек
+
+import UIKit
 
 final class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//frame - размеры относительно родительской вьюшки (x,y,h,w)
-//bounds = (x,y,h,w)
-        let viewTest =  UIView()
-// CGRect - Задаем координаты и размеры нашей вьюшки
-        viewTest.frame = CGRect(x: 40, y: 100, width: 100, height: 200)
-        viewTest.backgroundColor = .green
-        view.addSubview(viewTest)
-//    Constraints - позволяет задаваеть какаие отступы делать для вьюшек
-        
+    
     }
 }
