acec0c229c0e04336d9f9515cd893065afa593df
ze0ns <zeons@mail.ru>
2021-07-07 05:16:48 +0300

    Создали вьюшку, задали точки расположения и размеры

--- VK-Agent/Presentation/login/LoginViewController.swift
+++ VK-Agent/Presentation/login/LoginViewController.swift
@@ -11,35 +11,17 @@ import UIKit
 
 final class LoginViewController: UIViewController {
 
-    override func loadView() {
-        super.loadView()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        view.backgroundColor = .orange
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        view.backgroundColor = .cyan
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-    }
-    
-    
-    deinit {
+        
+//frame - размеры относительно родительской вьюшки (x,y,h,w)
+//bounds = (x,y,h,w)
+        let viewTest =  UIView()
+// CGRect - Задаем координаты и размеры нашей вьюшки
+        viewTest.frame = CGRect(x: 40, y: 100, width: 100, height: 200)
+        viewTest.backgroundColor = .green
+        view.addSubview(viewTest)
+//    Constraints - позволяет задаваеть какаие отступы делать для вьюшек
         
     }
 }
